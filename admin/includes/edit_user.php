86699591ebce0a7f01a6541af58d2d82dc51aee3
Jasurbek <jasurmamatqulov.uz@gmail.com>
2021-11-14 12:46:18 +0500

    hashed password

--- admin/includes/edit_user.php
+++ admin/includes/edit_user.php
@@ -35,6 +35,17 @@ if (isset($_POST['edit_user'])){
 
 //    move_uploaded_file($post_image_temp, "../images/$post_image");
 
+        $query = "SELECT randSalt FROM users " ;
+        $select_randsalt_query = mysqli_query($connection, $query);
+        if (!$select_randsalt_query){
+            die("Query Failed" . mysqli_error($connection));
+        }
+        $row = mysqli_fetch_array($select_randsalt_query);
+        $salt = $row['randSalt'];
+        $hashed_password = crypt($user_password, $salt);
+
+
+
 
 
     $query = "UPDATE users SET ";
@@ -43,7 +54,7 @@ if (isset($_POST['edit_user'])){
     $query .="user_role = '{$user_role}', ";
     $query .="username = '{$username}', ";
     $query .="user_email = '{$user_email}', ";
-    $query .="user_password = '{$user_password}' ";
+    $query .="user_password = '{$hashed_password}' ";
     $query .="WHERE user_id = {$the_user_id} ";
 
     $edit_user_query = mysqli_query($connection, $query);
@@ -76,7 +87,7 @@ if (isset($_POST['edit_user'])){
         <select name="user_role" id="">
 
 
-            <option value="subscriber"><?php  echo $user_role ?></option>
+            <option value="<?php  echo $user_role ?>"><?php  echo $user_role ?></option>
             <?php
             if ($user_role == 'admin'){
               echo  "<option value='subscriber'>subcriber</option>";
